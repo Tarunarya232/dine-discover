0ace7a32a3445970655507e72a139d988ab12482
Tarun <108832747+Tarunarya232@users.noreply.github.com>
2024-07-01 19:04:01 +0530

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import {
 import Home from "./routes/Home";
 import Update from "./routes/Update";
 import Details from "./routes/Details";
-import AddReview from "./components/AddReview"; // Import AddReview component
-import NotFound from "./components/NotFound"; // Import NotFound component
+import AddReview from "./components/AddReview";
+import NotFound from "./components/NotFound";
 import { RestaurantContextProvider } from './context/RestaurantContext';
 
 function App() {
